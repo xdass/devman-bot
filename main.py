bc7c60a5a83a9fd0632ac74370e04a4ff3f25c49
dass <420600base>
2019-05-13 16:40:03 +0500

    Fix missing attempts

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ from dotenv import load_dotenv
 def send_task_status(json_body):
     chat_id = os.getenv('chat_id')
     base_url = "https://dvmn.org"
-    lesson_title = json_body['new_attempts'][0]['lesson_title']
-    lesson_url = json_body['new_attempts'][0]['lesson_url']
-    is_negative = json_body['new_attempts'][0]['is_negative']
-    if is_negative:
-        message = f"Была проверена задача \"{lesson_title}\"\n\n Задача успешно решена!"
-    else:
-        message = f"Была проверена задача \"{lesson_title}\"" \
-            f"\n\n В задаче имеются ошибки. Посмотреть {base_url + lesson_url}"
-    bot.send_message(chat_id=chat_id, text=message)
+    attempts = json_body['new_attempts']
+    for attempt in attempts:
+        lesson_title = attempt['lesson_title']
+        lesson_url = attempt['lesson_url']
+        is_negative = attempt['is_negative']
+        if is_negative:
+            message = f"Была проверена задача \"{lesson_title}\"\n\n Задача успешно решена!"
+        else:
+            message = f"Была проверена задача \"{lesson_title}\"" \
+                f"\n\n В задаче имеются ошибки. Посмотреть {base_url + lesson_url}"
+        bot.send_message(chat_id=chat_id, text=message)
 
 
 def listen_polling():
@@ -29,18 +31,18 @@ def listen_polling():
     params = {'timestamp': start_timestamp}
     while True:
         try:
+            # time.sleep(60)
             response = requests.get(api_url, headers=headers, params=params, timeout=95)
-            if response.ok:
-                json_response = response.json()
-                status = json_response.get('status')
-                response_time = time.time()
-                if status == 'timeout':
-                    params = {'timestamp': response_time}
-                elif status == 'found':
-                    send_task_status(json_response)
-                    params = {'timestamp': response_time}
-            else:
-                raise requests.HTTPError(response)
+            response.raise_for_status()
+            json_response = response.json()
+            status = json_response.get('status')
+            if status == 'timeout':
+                params = {'timestamp': json_response.get('timestamp_to_request')}
+                print(json_response)
+            elif status == 'found':
+                # send_task_status(json_response)
+                print(json_response)
+                params = {'timestamp': json_response.get('last_attempt_timestamp')}
         except requests.exceptions.ConnectionError as e:
             print(f"Не удалось установить соединение с сервером, пробуем ещё раз...")
         except requests.exceptions.ReadTimeout as e:
