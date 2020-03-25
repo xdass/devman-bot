9c880476bb08c62d706ee311fb9baff2cb933ed3
Dmitry <420600@gmail.com>
2020-03-25 20:36:32 +0500

    Добавлен кастомный Exception, счетчик подключений

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
+import time
 import traceback
 import logging
 import requests
 from telegram.ext import Updater
 from dotenv import load_dotenv
+
+
+class SendStatusException(Exception):
+    pass
 
 
 class BotLogsHandler(logging.Handler):
@@ -14,13 +19,13 @@ class BotLogsHandler(logging.Handler):
         super().__init__()
 
     def emit(self, record):
-        chat_id = os.getenv('chat_id')
+        chat_id = os.getenv('CHAT_ID')
         log_entry = self.format(record)
         self.bot.send_message(chat_id=chat_id, text=log_entry)
 
 
 def send_task_status(json_body):
-    chat_id = os.getenv('chat_id')
+    chat_id = os.getenv('CHAT_ID')
     base_url = "https://dvmn.org"
     attempts = json_body['new_attempts']
     try:
@@ -29,18 +34,19 @@ def send_task_status(json_body):
             lesson_url = attempt['lesson_url']
             is_negative = attempt['is_negative']
             if is_negative:
-                message = f"Была проверена задача \"{lesson_title}\"" \
-                    f"\n\n В задаче имеются ошибки. Посмотреть {base_url + lesson_url}"
+                message = f"""Была проверена задача \"{lesson_title}\"
+                    В задаче имеются ошибки. Посмотреть {base_url + lesson_url}"""
             else:
-                message = f"Была проверена задача \"{lesson_title}\"\n\n Задача успешно решена!"
+                message = f"""Была проверена задача \"{lesson_title}\"
+                    Задача успешно решена!"""
             bot.send_message(chat_id=chat_id, text=message)
     except Exception as e:
-        logger.info(f"Бот упал с ошибкой:")
-        logger.info(traceback.format_exc())
+        raise SendStatusException(e)
 
 
 def listen_polling():
-    token = os.getenv('token')
+    token = os.getenv('DVMN_TOKEN')
+    connect_attempts = 3
     api_url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {token}'
@@ -58,17 +64,24 @@ def listen_polling():
                 send_task_status(json_response)
                 params = {'timestamp': json_response.get('last_attempt_timestamp')}
         except requests.exceptions.ConnectionError as e:
+            connect_attempts -= 1
             print(f"Не удалось установить соединение с сервером, пробуем ещё раз...")
+            if connect_attempts == 0:
+                print(f"Не удалось установить соединение с сервером, таймаут 30 секунд.")
+                time.sleep(30)
         except requests.exceptions.ReadTimeout as e:
             print(f"Ответ от сервера не получен, пробуем ещё раз...")
+        except SendStatusException:
+            logger.info(f"Бот упал с ошибкой:")
+            logger.info(traceback.format_exc())
 
 
 if __name__ == '__main__':
     load_dotenv()
-    bot_token = os.getenv('bot_token')
-    proxy_url = os.getenv('proxy')
-    proxy_login = os.getenv('proxy_login')
-    proxy_password = os.getenv('proxy_password')
+    bot_token = os.getenv('TELEGRAM_TOKEN')
+    proxy_url = os.getenv('PROXY')
+    proxy_login = os.getenv('PROXY_LOGIN')
+    proxy_password = os.getenv('PROXY_PASSWORD')
 
     logger = logging.getLogger("notify_bot")
     logger.setLevel(logging.INFO)
